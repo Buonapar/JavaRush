f9c424392fc183c13e6d1fae8ced5d000a8c4887
Alexey <alekseiovechkin@gmail.com>
2019-05-10 18:45:25 +0300

    Добавлен PartsController

---
+++ src/main/java/controller/PartController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import model.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import service.PartService;
+import service.PartServiceImpl;
+
+import java.util.List;
+
+@Controller
+public class PartController {
+
+    private  int page;
+
+    private PartService partService;
+
+    @Autowired
+    public void setPartService(PartService partService) {
+        this.partService = partService;
+    }
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView allParts(@RequestParam(defaultValue = "1") int page) {
+        this.page = page;
+        ModelAndView modelAndView = new ModelAndView();
+        List<Part> parts = partService.PartsList(page);
+        int partCount = partService.partsCount();
+        int pageCount = (partCount + 9) /10;
+        modelAndView.setViewName("parts");
+        modelAndView.addObject("page", page);
+        modelAndView.addObject("partsList", parts);
+        modelAndView.addObject("partCount", partCount);
+        modelAndView.addObject("pageCount", pageCount);
+        return modelAndView;
+    }
+}
--- src/main/webapp/WEB-INF/pages/parts.jsp
+++ src/main/webapp/WEB-INF/pages/parts.jsp
@@ -6,11 +6,24 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Список комплектующих</title>
 </head>
 <body>
-
+    <table>
+        <caption>Список комплектующих</caption>
+        <tr>
+            <th>Наименование</th>
+            <th>Необходимость</th>
+            <th>Количество</th>
+        </tr>
+        <c:forEach var="part" items="${partsList}" varStatus="i">
+            <tr>
+                <td>${i.index + 1 + (page - 1) * 10}</td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
